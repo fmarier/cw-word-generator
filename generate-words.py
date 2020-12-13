ac52c6a78b10d3a16700fbc38a005044e287687f
Francois Marier <francois@fmarier.org>
2020-12-12 16:32:24 -0800

    Add an option for the YnotSailing stages.

--- generate-words.py
+++ generate-words.py
@@ -24,6 +24,26 @@ import random
 import sys
 
 
+def characters(stages, extra_chars):
+    chars = extra_chars
+
+    # Add characters from each stage of ynotsailing.com course.
+    if '1' in stages:
+        chars += 'taeons8'
+    if '2' in stages:
+        chars += 'irh0dl2u'
+    if '3' in stages:
+        chars += 'cwf9my1'
+    if '4' in stages:
+        chars += 'gbvp37'
+    if '5' in stages:
+        chars += 'kjx4/5'
+    if '6' in stages:
+        chars += 'z56,q'
+
+    return chars
+
+
 def generate_word(chars, length):
     word = ""
     for dummy in range(length):
@@ -34,19 +54,26 @@ def generate_word(chars, length):
 def main():
     parser = argparse.ArgumentParser(description="CW word generator")
     parser.add_argument('characters', type=str, nargs='?',
-                        default='abcdefghijklmnopqrstuvwxyz1234567890.,?/@',
-                        help='all allowed characters')
+                        default='', help='all allowed characters')
     parser.add_argument('-l', '--length', dest='length',
                         type=int, default=5,
                         help='length of each word')
     parser.add_argument('-n', '--number', dest='number',
                         type=int, default=1,
                         help='number of words to generate')
+    parser.add_argument('-s', '--stages', dest='stages',
+                        type=str, default='',
+                        help='YnotSailing stages to include')
     args = parser.parse_args()
+
+    extra_chars = args.characters
+    if not extra_chars and not args.stages:
+        # Default to all characters on the Canadian exam.
+        extra_chars = 'abcdefghijklmnopqrstuvwxyz1234567890.,?/@'
 
     random.seed()
     for dummy in range(args.number):
-        print(generate_word(args.characters, args.length))
+        print(generate_word(characters(args.stages, extra_chars), args.length))
     return 0
 
 
